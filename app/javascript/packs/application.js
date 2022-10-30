6c48bb70b8b20d16a3fc8a9369e7a0cdeb28ff2d
edwinbbu <edwinbbu@gmail.com>
2022-10-30 13:46:44 +0530

    Resolved issues with ts setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,11 +18,7 @@
 
 import "stylesheets/application";
 
-// Support component names relative to this directory:
-var componentRequireContext = require.context("src", true);
-var ReactRailsUJS = require("react_ujs");
+import Rails from "@rails/ujs"
 
-// Makes React dev tools etc, work.
-ReactRailsUJS.mountComponents();
-
-ReactRailsUJS.useContext(componentRequireContext);
+Rails.start()
+ActiveStorage.start()
